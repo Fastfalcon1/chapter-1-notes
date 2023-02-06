4266324ab1527d2f2d050215fcef4d71a18f60df
6yhn <6yhn@fresnou.org>
2023-02-06 13:41:33 -0800

    initial commit

---
+++ datatypes.js
@@ -0,0 +1,34 @@
+console.log(9);
+
+//This is a comment
+
+//numbers
+//intergers
+console.log(-3);
+console.log(0);
+console.log(9);
+
+//floating point numbers (floats)
+console.log(3.14);
+console.log(-0.388327);
+
+//strings
+//characters, letters symbols, spaces
+console.log("hello world");
+console.log('$%@^& is also a string');
+console.log(`backticks by the 1`);
+
+//arithmetic operators (math)
+console.log(14+2);
+console.log(14-2);
+console.log(14*2);
+console.log(14/2);
+console.log(14%2);
+console.log(31%4);
+
+
+console.log("my name"+"is bullgoat.")
+console.log('my '+''+'age'+'is'+15'.')
+
+
+
